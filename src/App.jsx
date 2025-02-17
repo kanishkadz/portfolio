67c48507fdb29943b87e78e06d9a5a8e43711d50
Kanishka Anand <kanishkaadz@gmail.com>
2025-02-17 21:55:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
 
 const App = () => {
 
   return (
     <div>
-      <h1>Kanishka Anand's Portfolio</h1>
+      Kanishka Anand's Portfolio
     </div>
   )
 }
