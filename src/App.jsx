e1f72b29ecf0d7f6fa726e613fec6c62b4a8bfa8
Kanishka Anand <kanishkaadz@gmail.com>
2025-02-17 22:00:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,8 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
-          
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
