8710a367e27c1c8e2d62b468aec22b0178b2434d
Kanishka Anand <kanishkaadz@gmail.com>
2025-02-17 22:15:47 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/" className='flex items-center gap-2' onClick={() => {setActive(""); window.scrollTo(0, 0)}}>
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p>Kanishka Anand</p>
         </Link>
       </div>
     </nav>
