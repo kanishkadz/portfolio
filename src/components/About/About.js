7edcdfa4a4cdfcd83109a25449e4ea371ffad327
Kanishka Anand <kanishkaadz@gmail.com>
2025-07-08 22:33:58 +0530

    1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,9 +9,6 @@ import {
 } from "../styles/About.styled";
 import { SectionHeading, SectionTitle } from "../styles/SectionHeading";
 import { StyledParagraph } from "../styles/Typography.styled";
-import myImage from "../../assets/my-image.jpg";
-import downloadIcon from "../../assets/icons/download-icon.svg";
-// import resume from "../../assets/Resume.pdf";
 
 const About = () => {
   return (
