02ca84af15e6b4a2c621c92f787b5cce6f22776b
Kanishka Anand <kanishkaadz@gmail.com>
2025-07-08 21:50:45 +0530

    1

--- src/components/Playground/data.js
+++ src/components/Playground/data.js
@@ -1,24 +1,16 @@
-import enhance from "../../assets/playground/enhance.png";
 import gemini from "../../assets/playground/gemini.png";
 import quick from "../../assets/playground/quick.png";
 
 export const projectsData = [
   {
     id: 1,
-    projectName: "Enhance AI",
-    liveLink: "https://enhance-ai-sandy.vercel.app/",
-    hasLink: true,
-    img: enhance,
-  },
-  {
-    id: 2,
     projectName: "Gemini Clone",
     liveLink: "https://gemini-clone-eight-eta.vercel.app/",
     hasLink: true,
     img: gemini,
   },
   {
-    id: 3,
+    id: 2,
     projectName: "Quick Bites",
     hasLink: true,
     liveLink: "https://quick-bites-five.vercel.app/",
